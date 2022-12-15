d1bd7171bb249e2fd2b0b31e25f767fce9e393c5
lin9587 <lin9587@aliyun.com>
2022-12-15 15:21:08 +0800

    fix: packing-object file

--- 02day/src/4.packingObject.ts
+++ 02day/src/4.packingObject.ts
@@ -1,11 +1,11 @@
-let a: string;
-a = 'lin';
-a = new string('1');
+// let a: string;
+// a = 'lin';
+// a = new string('1');
 
 
-let b: String;
-b = new String('lin');
-b = 'lin'
+// let b: String;
+// b = new String('lin');
+// b = 'lin'
 
 
 
