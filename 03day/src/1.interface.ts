4ee062e401f6d2950526b23f9f4cdc5fb0bca4d5
lin9587 <lin9587@aliyun.com>
2022-12-17 14:24:02 +0800

    feat: leran interface

---
+++ 03day/src/1.interface.ts
@@ -0,0 +1,23 @@
+// interface Point {
+//     x: number,
+//     y: number
+// }
+
+// let p1: Point = {
+//     x: 100,
+//     y: 100
+// }
+
+// 不能作为值使用
+// let p2 = Pinot; // error
+
+
+
+
+
+
+
+
+
+
+
